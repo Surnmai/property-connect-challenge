c6c90f36764afa013a83706ba7810c09e685ccd7
Surnmai <surnmai1@gmail.com>
2025-03-12 12:52:45 +0000

    hero changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,9 @@ const Hero = () => {
       <section className="w-full h-screen bg-hero-img bg-no-repeat bg-cover bg-center">
         {/* <div className="w-full h-[39.7rem] bg-blackOverlay py-8"> */}
         <div className="container">
-          <p className="text-white text-4xl font-bold ">Hero</p>
+          <p className="text-white text-4xl lg:text-8xl font-bold translate-y-60">
+            Buy Your <br /> <span className="pl-16">Dream Property</span>
+          </p>
         </div>
         {/* </div> */}
       </section>
--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -37,7 +37,7 @@ const Properties = () => {
           />
 
           {/* Properties list  */}
-          <PropertiesCard items={items} />
+          <PropertiesCard items={items} key={items.id} />
         </div>
       </section>
     </>
