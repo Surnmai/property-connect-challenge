7ec59716146181d2e98b547a9017b30d28ef334b
Surnmai <surnmai1@gmail.com>
2025-03-12 12:28:12 +0000

    filterBtn Component changed

--- src/components/FilterBtn.jsx
+++ src/components/FilterBtn.jsx
@@ -10,6 +10,13 @@ const FilterBtn = ({ filteredBtn, allData, filteredItems }) => {
   return (
     <>
       <div className="flex justify-center gap-1 mb-4">
+        {/* all btn  */}
+        <button
+          className="capitalize bg-primary rounded-md px-4 py-2 hover:text-white transition-all duration-500"
+          onClick={() => allData(properties)}
+        >
+          all
+        </button>
         {filteredBtn.map((btn) => (
           <button
             className="capitalize bg-primary rounded-md px-4 py-2 hover:text-white transition-all duration-500"
@@ -18,13 +25,6 @@ const FilterBtn = ({ filteredBtn, allData, filteredItems }) => {
             {btn}
           </button>
         ))}
-        {/* all btn  */}
-        <button
-          className="capitalize bg-primary rounded-md px-4 py-2 hover:text-white transition-all duration-500"
-          onClick={() => allData(properties)}
-        >
-          all
-        </button>
       </div>
     </>
   );
